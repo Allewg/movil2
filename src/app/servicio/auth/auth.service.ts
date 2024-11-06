eb77600319c30f1d77b55e7fb5940f6db5fb262c
Gonzalo Mella Díaz <gmella@macrofood.cl>
2024-11-06 17:40:47 -0300

    Creacion de servicio login completo

--- src/app/servicio/auth/auth.service.ts
+++ src/app/servicio/auth/auth.service.ts
@@ -1,27 +1,51 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UsuarioLogeado } from 'src/app/interfaces/UsuarioLogeado';
+import { CuerpoLogin } from 'src/app/interfaces/CuerpoLogin';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login'
+
+private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login';
+public usuarioLogeado : UsuarioLogeado | null = null;
+public accessToken: string | null = null;
+
+private $cargando = new BehaviorSubject<boolean>(false);
+public cargando = this.$cargando.asObservable();
+
+
   constructor(
     private http: HttpClient
   ) { }
 public iniciarSesion(nombre_usuario: string, contrasenia:string){
+this.$cargando.next(true);
+
   const cuerpo = {
     username: nombre_usuario,
     password: contrasenia
   }
-  this.http.post(this.URL_LOGIN,JSON.stringify(cuerpo), {
+  this.http.post<UsuarioLogeado>(this.URL_LOGIN,JSON.stringify(cuerpo), {
     headers: {
       'Content-Type': 'application/json'
     }
   })
   .subscribe(resultado => {
+    this.accessToken = resultado.accessToken
+    this.usuarioLogeado = resultado;
+    this.$cargando.next(false);
     console.log(resultado);
   });
+
 }
+
+public cerrarSesion() {
+  if(this.usuarioLogeado){
+    this.usuarioLogeado = null;
+    this.accessToken = null;
+  }
+  }
 
 
 }
